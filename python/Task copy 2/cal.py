10dcd557169e0430cd210a3001617b7e81facf59
aliamandevelo-boop <aliamandevelo@gmail.com>
2026-09-04 14:28:58 +0530

    my  code

---
+++ python/Task copy 2/cal.py
@@ -0,0 +1,34 @@
+try:
+    a = int(input("Enter first value: "))
+    b = int(input("Enter second value: "))
+
+    print("\n1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("Answer =", a + b)
+
+    elif choice == 2:
+        print("Answer =", a - b)
+
+    elif choice == 3:
+        print("Answer =", a * b)
+
+    elif choice == 4:
+        if b == 0:
+            print("Cannot divide by zero!")
+        else:
+            print("Answer =", a / b)
+
+    else:
+        print("Wrong choice! Please enter 1 to 4.")
+
+except ValueError:
+    print("Wrong input! Only numbers are allowed.")
+
+
+    
---
+++ python/aman.py
@@ -0,0 +1,7 @@
+age = input("Enter your age: ")
+
+new_age = int(age) + 1
+
+print(new_age)
+
+print(float(new_age))
